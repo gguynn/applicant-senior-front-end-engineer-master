072742584c9216cd8eb61ee3fed478ad73abf0ea
Garrett <garrett@gguynn.com>
2020-08-06 13:09:26 -1000

    Exported WP SQL files to src.

--- src/eats_2020-08-06.sql
+++ src/eats_2020-08-06.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.29)
 # Database: eats
-# Generation Time: 2020-08-06 22:38:25 +0000
+# Generation Time: 2020-08-06 23:08:59 +0000
 # ************************************************************
 
 
@@ -234,10 +234,8 @@ VALUES
 	(110,'widget_tag_cloud','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
 	(111,'widget_nav_menu','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
 	(112,'widget_custom_html','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-	(113,'_transient_doing_cron','1596753341.6487109661102294921875','yes'),
-	(114,'_site_transient_update_core','O:8:\"stdClass\":4:{s:7:\"updates\";a:1:{i:0;O:8:\"stdClass\":10:{s:8:\"response\";s:6:\"latest\";s:8:\"download\";s:59:\"https://downloads.wordpress.org/release/wordpress-5.4.2.zip\";s:6:\"locale\";s:5:\"en_US\";s:8:\"packages\";O:8:\"stdClass\":5:{s:4:\"full\";s:59:\"https://downloads.wordpress.org/release/wordpress-5.4.2.zip\";s:10:\"no_content\";s:70:\"https://downloads.wordpress.org/release/wordpress-5.4.2-no-content.zip\";s:11:\"new_bundled\";s:71:\"https://downloads.wordpress.org/release/wordpress-5.4.2-new-bundled.zip\";s:7:\"partial\";b:0;s:8:\"rollback\";b:0;}s:7:\"current\";s:5:\"5.4.2\";s:7:\"version\";s:5:\"5.4.2\";s:11:\"php_version\";s:6:\"5.6.20\";s:13:\"mysql_version\";s:3:\"5.0\";s:11:\"new_bundled\";s:3:\"5.3\";s:15:\"partial_version\";s:0:\"\";}}s:12:\"last_checked\";i:1596753371;s:15:\"version_checked\";s:5:\"5.4.2\";s:12:\"translations\";a:0:{}}','no'),
-	(116,'_site_transient_timeout_theme_roots','1596754207','no'),
-	(117,'_site_transient_theme_roots','a:3:{s:14:\"twentynineteen\";s:7:\"/themes\";s:15:\"twentyseventeen\";s:7:\"/themes\";s:12:\"twentytwenty\";s:7:\"/themes\";}','no'),
+	(113,'_transient_doing_cron','1596755248.5071589946746826171875','yes'),
+	(114,'_site_transient_update_core','O:8:\"stdClass\":4:{s:7:\"updates\";a:1:{i:0;O:8:\"stdClass\":10:{s:8:\"response\";s:6:\"latest\";s:8:\"download\";s:59:\"https://downloads.wordpress.org/release/wordpress-5.4.2.zip\";s:6:\"locale\";s:5:\"en_US\";s:8:\"packages\";O:8:\"stdClass\":5:{s:4:\"full\";s:59:\"https://downloads.wordpress.org/release/wordpress-5.4.2.zip\";s:10:\"no_content\";s:70:\"https://downloads.wordpress.org/release/wordpress-5.4.2-no-content.zip\";s:11:\"new_bundled\";s:71:\"https://downloads.wordpress.org/release/wordpress-5.4.2-new-bundled.zip\";s:7:\"partial\";b:0;s:8:\"rollback\";b:0;}s:7:\"current\";s:5:\"5.4.2\";s:7:\"version\";s:5:\"5.4.2\";s:11:\"php_version\";s:6:\"5.6.20\";s:13:\"mysql_version\";s:3:\"5.0\";s:11:\"new_bundled\";s:3:\"5.3\";s:15:\"partial_version\";s:0:\"\";}}s:12:\"last_checked\";i:1596755176;s:15:\"version_checked\";s:5:\"5.4.2\";s:12:\"translations\";a:0:{}}','no'),
 	(119,'_site_transient_timeout_browser_4c32a719821394961a4ade1553afbd48','1597357208','no'),
 	(120,'_site_transient_browser_4c32a719821394961a4ade1553afbd48','a:10:{s:4:\"name\";s:6:\"Chrome\";s:7:\"version\";s:12:\"84.0.4147.89\";s:8:\"platform\";s:9:\"Macintosh\";s:10:\"update_url\";s:29:\"https://www.google.com/chrome\";s:7:\"img_src\";s:43:\"http://s.w.org/images/browsers/chrome.png?1\";s:11:\"img_src_ssl\";s:44:\"https://s.w.org/images/browsers/chrome.png?1\";s:15:\"current_version\";s:2:\"18\";s:7:\"upgrade\";b:0;s:8:\"insecure\";b:0;s:6:\"mobile\";b:0;}','no'),
 	(121,'_site_transient_timeout_php_check_f9714bbe413cc376a70847d9c1f86fcc','1597357208','no'),
@@ -258,8 +256,10 @@ VALUES
 	(133,'_transient_feed_mod_d117b5738fbd35bd8c0391cda1f2b5d9','1596752410','no'),
 	(134,'_transient_timeout_dash_v2_88ae138922fe95674369b1cb3d215a2b','1596795610','no'),
 	(135,'_transient_dash_v2_88ae138922fe95674369b1cb3d215a2b','<div class=\"rss-widget\"><ul><li><a class=\'rsswidget\' href=\'https://wordpress.org/news/2020/08/wordpress-5-5-release-candidate-2/\'>WordPress 5.5 Release Candidate 2</a></li></ul></div><div class=\"rss-widget\"><ul><li><a class=\'rsswidget\' href=\'https://wptavern.com/automattic-relaunches-p2-self-hosted-version-on-the-roadmap?utm_source=rss&#038;utm_medium=rss&#038;utm_campaign=automattic-relaunches-p2-self-hosted-version-on-the-roadmap\'>WPTavern: Automattic Relaunches P2, Self-Hosted Version on the Roadmap</a></li><li><a class=\'rsswidget\' href=\'https://wptavern.com/plugin-rank-provides-insight-into-wordpress-search-results-competitive-analysis-and-email-reports-for-developers?utm_source=rss&#038;utm_medium=rss&#038;utm_campaign=plugin-rank-provides-insight-into-wordpress-search-results-competitive-analysis-and-email-reports-for-developers\'>WPTavern: Plugin Rank Provides Insight Into WordPress Search Results, Competitive Analysis, and Email Reports for Developers</a></li><li><a class=\'rsswidget\' href=\'https://buddypress.org/2020/08/buddypress-6-2-0/\'>BuddyPress: BuddyPress 6.2.0 Maintenance release</a></li></ul></div>','no'),
-	(136,'_site_transient_update_themes','O:8:\"stdClass\":4:{s:12:\"last_checked\";i:1596753393;s:7:\"checked\";a:3:{s:14:\"twentynineteen\";s:3:\"1.6\";s:15:\"twentyseventeen\";s:3:\"2.3\";s:12:\"twentytwenty\";s:3:\"1.4\";}s:8:\"response\";a:0:{}s:12:\"translations\";a:0:{}}','no'),
-	(137,'_site_transient_update_plugins','O:8:\"stdClass\":4:{s:12:\"last_checked\";i:1596753392;s:8:\"response\";a:0:{}s:12:\"translations\";a:0:{}s:9:\"no_update\";a:2:{s:19:\"akismet/akismet.php\";O:8:\"stdClass\":9:{s:2:\"id\";s:21:\"w.org/plugins/akismet\";s:4:\"slug\";s:7:\"akismet\";s:6:\"plugin\";s:19:\"akismet/akismet.php\";s:11:\"new_version\";s:5:\"4.1.6\";s:3:\"url\";s:38:\"https://wordpress.org/plugins/akismet/\";s:7:\"package\";s:56:\"https://downloads.wordpress.org/plugin/akismet.4.1.6.zip\";s:5:\"icons\";a:2:{s:2:\"2x\";s:59:\"https://ps.w.org/akismet/assets/icon-256x256.png?rev=969272\";s:2:\"1x\";s:59:\"https://ps.w.org/akismet/assets/icon-128x128.png?rev=969272\";}s:7:\"banners\";a:1:{s:2:\"1x\";s:61:\"https://ps.w.org/akismet/assets/banner-772x250.jpg?rev=479904\";}s:11:\"banners_rtl\";a:0:{}}s:9:\"hello.php\";O:8:\"stdClass\":9:{s:2:\"id\";s:25:\"w.org/plugins/hello-dolly\";s:4:\"slug\";s:11:\"hello-dolly\";s:6:\"plugin\";s:9:\"hello.php\";s:11:\"new_version\";s:5:\"1.7.2\";s:3:\"url\";s:42:\"https://wordpress.org/plugins/hello-dolly/\";s:7:\"package\";s:60:\"https://downloads.wordpress.org/plugin/hello-dolly.1.7.2.zip\";s:5:\"icons\";a:2:{s:2:\"2x\";s:64:\"https://ps.w.org/hello-dolly/assets/icon-256x256.jpg?rev=2052855\";s:2:\"1x\";s:64:\"https://ps.w.org/hello-dolly/assets/icon-128x128.jpg?rev=2052855\";}s:7:\"banners\";a:1:{s:2:\"1x\";s:66:\"https://ps.w.org/hello-dolly/assets/banner-772x250.jpg?rev=2052855\";}s:11:\"banners_rtl\";a:0:{}}}}','no');
+	(136,'_site_transient_update_themes','O:8:\"stdClass\":4:{s:12:\"last_checked\";i:1596755176;s:7:\"checked\";a:3:{s:14:\"twentynineteen\";s:3:\"1.5\";s:15:\"twentyseventeen\";s:3:\"2.3\";s:12:\"twentytwenty\";s:3:\"1.2\";}s:8:\"response\";a:2:{s:14:\"twentynineteen\";a:6:{s:5:\"theme\";s:14:\"twentynineteen\";s:11:\"new_version\";s:3:\"1.6\";s:3:\"url\";s:44:\"https://wordpress.org/themes/twentynineteen/\";s:7:\"package\";s:60:\"https://downloads.wordpress.org/theme/twentynineteen.1.6.zip\";s:8:\"requires\";s:5:\"4.9.6\";s:12:\"requires_php\";s:5:\"5.2.4\";}s:12:\"twentytwenty\";a:6:{s:5:\"theme\";s:12:\"twentytwenty\";s:11:\"new_version\";s:3:\"1.4\";s:3:\"url\";s:42:\"https://wordpress.org/themes/twentytwenty/\";s:7:\"package\";s:58:\"https://downloads.wordpress.org/theme/twentytwenty.1.4.zip\";s:8:\"requires\";s:3:\"4.7\";s:12:\"requires_php\";s:5:\"5.2.4\";}}s:12:\"translations\";a:0:{}}','no'),
+	(137,'_site_transient_update_plugins','O:8:\"stdClass\":5:{s:12:\"last_checked\";i:1596755178;s:7:\"checked\";a:2:{s:19:\"akismet/akismet.php\";s:5:\"4.1.5\";s:9:\"hello.php\";s:5:\"1.7.2\";}s:8:\"response\";a:1:{s:19:\"akismet/akismet.php\";O:8:\"stdClass\":12:{s:2:\"id\";s:21:\"w.org/plugins/akismet\";s:4:\"slug\";s:7:\"akismet\";s:6:\"plugin\";s:19:\"akismet/akismet.php\";s:11:\"new_version\";s:5:\"4.1.6\";s:3:\"url\";s:38:\"https://wordpress.org/plugins/akismet/\";s:7:\"package\";s:56:\"https://downloads.wordpress.org/plugin/akismet.4.1.6.zip\";s:5:\"icons\";a:2:{s:2:\"2x\";s:59:\"https://ps.w.org/akismet/assets/icon-256x256.png?rev=969272\";s:2:\"1x\";s:59:\"https://ps.w.org/akismet/assets/icon-128x128.png?rev=969272\";}s:7:\"banners\";a:1:{s:2:\"1x\";s:61:\"https://ps.w.org/akismet/assets/banner-772x250.jpg?rev=479904\";}s:11:\"banners_rtl\";a:0:{}s:6:\"tested\";s:5:\"5.4.2\";s:12:\"requires_php\";b:0;s:13:\"compatibility\";O:8:\"stdClass\":0:{}}}s:12:\"translations\";a:0:{}s:9:\"no_update\";a:1:{s:9:\"hello.php\";O:8:\"stdClass\":9:{s:2:\"id\";s:25:\"w.org/plugins/hello-dolly\";s:4:\"slug\";s:11:\"hello-dolly\";s:6:\"plugin\";s:9:\"hello.php\";s:11:\"new_version\";s:5:\"1.7.2\";s:3:\"url\";s:42:\"https://wordpress.org/plugins/hello-dolly/\";s:7:\"package\";s:60:\"https://downloads.wordpress.org/plugin/hello-dolly.1.7.2.zip\";s:5:\"icons\";a:2:{s:2:\"2x\";s:64:\"https://ps.w.org/hello-dolly/assets/icon-256x256.jpg?rev=2052855\";s:2:\"1x\";s:64:\"https://ps.w.org/hello-dolly/assets/icon-128x128.jpg?rev=2052855\";}s:7:\"banners\";a:1:{s:2:\"1x\";s:66:\"https://ps.w.org/hello-dolly/assets/banner-772x250.jpg?rev=2052855\";}s:11:\"banners_rtl\";a:0:{}}}}','no'),
+	(138,'_site_transient_timeout_theme_roots','1596756975','no'),
+	(139,'_site_transient_theme_roots','a:3:{s:14:\"twentynineteen\";s:7:\"/themes\";s:15:\"twentyseventeen\";s:7:\"/themes\";s:12:\"twentytwenty\";s:7:\"/themes\";}','no');
 
 /*!40000 ALTER TABLE `wp_options` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -472,9 +472,22 @@ VALUES
 	(13,1,'wp_user_level','10'),
 	(14,1,'dismissed_wp_pointers',''),
 	(15,1,'show_welcome_panel','1'),
-	(16,1,'session_tokens','a:1:{s:64:\"561bebd6e3bed2fd34f49b6ddd9abffe930f17ffae3a8b4f3de3d1a894308f63\";a:4:{s:10:\"expiration\";i:1596925206;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:120:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36\";s:5:\"login\";i:1596752406;}}'),
 	(17,1,'wp_dashboard_quick_press_last_post_id','4'),
-	(18,1,'community-events-location','a:1:{s:2:\"ip\";s:9:\"127.0.0.0\";}');
+	(18,1,'community-events-location','a:1:{s:2:\"ip\";s:9:\"127.0.0.0\";}'),
+	(19,2,'nickname','demo'),
+	(20,2,'first_name',''),
+	(21,2,'last_name',''),
+	(22,2,'description',''),
+	(23,2,'rich_editing','true'),
+	(24,2,'syntax_highlighting','true'),
+	(25,2,'comment_shortcuts','false'),
+	(26,2,'admin_color','fresh'),
+	(27,2,'use_ssl','0'),
+	(28,2,'show_admin_bar_front','true'),
+	(29,2,'locale',''),
+	(30,2,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}'),
+	(31,2,'wp_user_level','10'),
+	(32,2,'dismissed_wp_pointers','');
 
 /*!40000 ALTER TABLE `wp_usermeta` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -507,7 +520,8 @@ LOCK TABLES `wp_users` WRITE;
 
 INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`)
 VALUES
-	(1,'gguynn','$P$B3qREok.56KDMamXS6vyBFza0bT3rR0','gguynn','garrett@gguynn.com','http://eats.test','2020-08-06 22:19:54','',0,'gguynn');
+	(1,'gguynn','$P$B3qREok.56KDMamXS6vyBFza0bT3rR0','gguynn','garrett@gguynn.com','http://eats.test','2020-08-06 22:19:54','',0,'gguynn'),
+	(2,'demo','$P$BakxHv2.DFkm0yp2WUMhmWJn4SGGsH0','demo','demo@gguynn.com','','2020-08-06 23:06:53','1596755213:$P$BbVxybvtbXc96HoDsi8nRqVubCYERl1',0,'demo');
 
 /*!40000 ALTER TABLE `wp_users` ENABLE KEYS */;
 UNLOCK TABLES;
